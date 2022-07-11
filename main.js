43621f0dbc31754809ba5f2bbfe5414077b0d3b6
Randhir200 <mail.randhirkr@gmail.com>
2022-07-11 23:35:17 +0530

    done

---
+++ main.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 4, 5, 6];
+let n = 4;
+let binarySearch = (arr,low, high, n) => {
+    let mid = Math.floor((low+high)/2)//2
+    if(arr[mid]===n){
+        console.log(`found at position ${mid}`);
+        return;
+    }
+    if(arr[mid]<n){
+        low = mid;
+    }
+    if(arr[mid]>n){
+        high = mid;
+    }
+    binarySearch(arr, low, high,n);
+}
+    binarySearch(arr,0,arr.length, n)
