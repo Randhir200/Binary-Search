73deedb56a1918f91dfff3285c33e4d7e88a5a7e
Randhir200 <mail.randhirkr@gmail.com>
2022-07-16 01:00:48 +0530

    done

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
-let arr = [1, 2, 3, 4, 5, 6];
-let n = 6;
-let binarySearch = (arr,low, high, n) => {
-    let mid = Math.floor(low+(high-low)/2)//2
-    if(arr[mid]===n){
-        console.log(`found at position ${mid}`);
-        return;
+//Binary Search - Iterative
+function runProgram(input) {
+    input = input.trim().split("\n");
+    let [n,k] = input[0].trim().split(" ").map(Number);
+    let arr = input[1].trim().split(" ").map(Number);
+    let flag = false;
+    let left = 0;
+    let right = n-1;
+    let mid;
+    arr.sort((a,b)=>a-b);
+    //2 -2 0 3 4
+    function search(){
+        while(left<=right){ //0!=4&0!=undefined
+         mid = calMid(left, right);
+        if(k>arr[mid]){ //
+            left = mid+1;//
+        }
+        if(k<arr[mid]){
+            right = mid-1
+        }
+        if(k==arr[mid]){
+            return 1;
+        }
     }
-    if(arr[mid]<n){
-        low = mid;
+    return -1
     }
-    if(arr[mid]>n){
-        high = mid;
+    
+    function calMid(left, right){
+        return Math.floor(left + (right-left)/2);
     }
-    binarySearch(arr, low, high,n);
-}
-    binarySearch(arr,0,arr.length, n)
+    console.log(search(left, right))
+  
+  }
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
+  
